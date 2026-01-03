e3bd22314829bae05f705ad00c9f3678f17ac031
unknown <christophervalero05@hotmail.com>
2026-01-03 13:49:28 -0600

    cambios en main de banners

--- src/components/BarraNaranja.jsx
+++ src/components/BarraNaranja.jsx
@@ -4,18 +4,18 @@ const BarraNaranja = () => {
   return (
     <div className="w-full bg-orange-500 h-32 sm:h-24 relative overflow-hidden flex items-center justify-center py-2 px-4">
       {/* Banner para desktop - se oculta en móvil */}
-      <img 
+      {/* <img 
         src="/images/Banner LAB.png" 
         alt="Banner LAB"
         className="hidden sm:block h-full w-auto object-contain"
-      />
+      /> */}
       
       {/* Banner para móvil - más grande */}
-      <img 
+      {/* <img 
         src="/images/Banner LAB-movil.png" 
         alt="Banner LAB"
         className="block sm:hidden h-full w-auto object-contain"
-      />
+      /> */}
     </div>
   )
 }
